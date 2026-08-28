e5bc80676c04b6af761e5e9eff0bb985be9f1fa8
Bruno Paulon <143882052+bfrpaulondev@users.noreply.github.com>
2026-08-28 13:26:46 +0100

    feat(m7): wire camera inspection zoom

--- src/game/ApartmentSkeleton.tsx
+++ src/game/ApartmentSkeleton.tsx
@@ -2,6 +2,7 @@ import { PointerLockControls } from '@react-three/drei'
 import { ApartmentScene } from './ApartmentScene'
 import { AudioAmbience } from './audio/AudioAmbience'
 import { InteractionSystem } from './interaction/InteractionSystem'
+import { CameraPolish } from './player/CameraPolish'
 import { PlayerController } from './player/PlayerController'
 import { APARTMENT_COLLIDERS } from './physics/colliders'
 import { useGameStore } from './state/gameStore'
@@ -22,23 +23,23 @@ export function ApartmentSkeleton({
   const cinematic = useGameStore((state) => state.cinematic)
   const blackout = useGameStore((state) => state.blackout)
   const demoEnded = useGameStore((state) => state.demoEnded)
+  const cameraInteractionEnabled =
+    gameStarted &&
+    isPointerLocked &&
+    !noteOpen &&
+    !cinematic &&
+    !blackout &&
+    !demoEnded
 
   return (
     <>
       <ApartmentScene />
       <AudioAmbience enabled={gameStarted && !demoEnded} />
       {gameStarted && <InteractionSystem />}
+      <CameraPolish enabled={cameraInteractionEnabled} />
       <PlayerController
         colliders={APARTMENT_COLLIDERS}
-        enabled={
-          gameStarted &&
-          isPointerLocked &&
-          awake &&
-          !noteOpen &&
-          !cinematic &&
-          !blackout &&
-          !demoEnded
-        }
+        enabled={cameraInteractionEnabled && awake}
       />
 
       {gameStarted && !demoEnded && (
