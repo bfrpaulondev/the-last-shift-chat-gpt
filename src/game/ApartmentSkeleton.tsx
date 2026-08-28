97429c948d05ac99be2da150d12f5901c1c47c7f
Bruno Paulon <143882052+bfrpaulondev@users.noreply.github.com>
2026-08-28 12:50:36 +0100

    feat(m3): wire interactables into apartment scene

--- src/game/ApartmentSkeleton.tsx
+++ src/game/ApartmentSkeleton.tsx
@@ -1,6 +1,8 @@
 import { PointerLockControls } from '@react-three/drei'
+import { InteractionSystem } from './interaction/InteractionSystem'
 import { PlayerController } from './player/PlayerController'
 import { APARTMENT_COLLIDERS } from './physics/colliders'
+import { useGameStore } from './state/gameStore'
 
 type ApartmentSkeletonProps = {
   isPointerLocked: boolean
@@ -14,6 +16,8 @@ export function ApartmentSkeleton({
   isPointerLocked,
   onLockChange,
 }: ApartmentSkeletonProps) {
+  const noteOpen = useGameStore((state) => Boolean(state.note))
+
   return (
     <>
       <ambientLight color="#aeb8c8" intensity={0.35} />
@@ -53,7 +57,40 @@ export function ApartmentSkeleton({
         </mesh>
       </group>
 
-      <PlayerController colliders={APARTMENT_COLLIDERS} enabled={isPointerLocked} />
+      <mesh
+        castShadow
+        receiveShadow
+        position={[-1.45, 0.3, 1.15]}
+        userData={{ interactableId: 'bed' }}
+      >
+        <boxGeometry args={[2.1, 0.6, 1.3]} />
+        <meshStandardMaterial color="#55565a" roughness={1} />
+      </mesh>
+
+      <mesh
+        castShadow
+        position={[0.7, 1.85, -2.88]}
+        userData={{ interactableId: 'clock' }}
+      >
+        <boxGeometry args={[0.42, 0.42, 0.08]} />
+        <meshStandardMaterial color="#25282c" roughness={0.9} />
+      </mesh>
+
+      <mesh
+        castShadow
+        position={[2.2, 1.45, 2.88]}
+        rotation={[0, Math.PI, 0]}
+        userData={{ interactableId: 'frame' }}
+      >
+        <boxGeometry args={[0.7, 0.9, 0.07]} />
+        <meshStandardMaterial color="#3a3028" roughness={0.95} />
+      </mesh>
+
+      <InteractionSystem />
+      <PlayerController
+        colliders={APARTMENT_COLLIDERS}
+        enabled={isPointerLocked && !noteOpen}
+      />
 
       <PointerLockControls
         makeDefault
